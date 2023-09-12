ca59f6a96d610b5d2111ac87174aa104476c00de
Александр Гавриленко <fruzexshow@gmail.com>
2023-09-12 16:26:17 +0700

    Fix review 12

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,5 +1,6 @@
-from rest_framework.routers import DefaultRouter
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
 from api.views import (FavoriteAPIView, IngredientViewSet, RecipeViewSet,
                        ShoppingCartAPIView, SubscriptionsListAPIView,
                        TagViewSet, UserSubscriptionsAPIView)
