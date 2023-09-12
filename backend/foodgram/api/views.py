90066d90df376c68dae0787b8ab8a45a4190ebb5
Александр Гавриленко <fruzexshow@gmail.com>
2023-09-12 17:06:10 +0700

    Fix pep8 3

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -5,7 +5,6 @@ from rest_framework import mixins, status, views, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import SAFE_METHODS, IsAuthenticated
 from rest_framework.response import Response
-
 from api.filters import IngredientFilter
 from api.permissions import IsAdminAuthorOrReadOnly, IsAdminReadOnly
 from api.serializers import (IngredientSerializer, RecipeCreateSerializer,
